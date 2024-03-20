bd023a4b09fbb3409c93f57aed005fb9cde03b9a
ERDesigns Software <ernstreidinga85@gmail.com>
2024-03-20 16:58:34 +0100

    Progress: Visual
    Created a base component that uses buffering to avoid/minimize flickering, and draws to the control canvas at specified FPS. And started on the basis for a Gauge component.

---
+++ OBD.CircularGauge.pas
@@ -0,0 +1,243 @@
+﻿//------------------------------------------------------------------------------
+// UNIT           : OBD.CircularGauge.pas
+// CONTENTS       : Circular gauge component
+// VERSION        : 1.0
+// TARGET         : Embarcadero Delphi 11 or higher
+// AUTHOR         : Ernst Reidinga (ERDesigns)
+// STATUS         : Open source under Apache 2.0 library
+// COMPATIBILITY  : Windows 7, 8/8.1, 10, 11
+// RELEASE DATE   : 20/03/2024
+//------------------------------------------------------------------------------
+unit OBD.CircularGauge;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Vcl.Controls, WinApi.Windows, Winapi.Messages,
+  Vcl.Graphics, OBD.CustomControl;
+
+//------------------------------------------------------------------------------
+// CONSTANTS
+//------------------------------------------------------------------------------
+const
+  /// <summary>
+  ///   Default start angle: Starting from bottom left
+  /// </summary>
+  DEFAULT_START_ANGLE = 135;
+  /// <summary>
+  ///   Default end angle: Ending at bottom right
+  /// </summary>
+  DEFAULT_END_ANGLE = 225;
+  /// <summary>
+  ///   Minimum starting angle
+  /// </summary>
+  MIN_START_ANGLE: Single = 0;
+  /// <summary>
+  ///   Minimum ending angle
+  /// </summary>
+  MIN_END_ANGLE: Single = 0;
+  /// <summary>
+  ///   Maximum starting angle, allowing a full circle
+  /// </summary>
+  MAX_START_ANGLE: Single = 360;
+  /// <summary>
+  ///   Maximum ending angle, allowing a full circle
+  /// </summary>
+  MAX_END_ANGLE: Single = 360;
+
+//------------------------------------------------------------------------------
+// CLASSES
+//------------------------------------------------------------------------------
+type
+  TOBDCircularGauge = class(TOBDCustomControl)
+  private
+    /// <summary>
+    ///   Background Buffer
+    /// </summary>
+    FBackgroundBuffer: TBitmap;
+  private
+    /// <summary>
+    ///   Start angle
+    /// </summary>
+    FStartAngle: Single;
+    /// <summary>
+    ///   End angle
+    /// </summary>
+    FEndAngle: Single;
+
+    /// <summary>
+    ///   Set start angle
+    /// </summary>
+    procedure SetStartAngle(Value: Single);
+    /// <summary>
+    ///   Set end angle
+    /// </summary>
+    procedure SetEndAngle(Value: Single);
+  protected
+    /// <summary>
+    ///   Invalidate background (Repaint background buffer)
+    /// </summary>
+    procedure InvalidateBackground; virtual;
+    /// <summary>
+    ///   Paint buffer
+    /// </summary>
+    procedure PaintBuffer; override;
+  public
+    /// <summary>
+    ///   Constructor
+    /// </summary>
+    constructor Create(AOwner: TComponent); override;
+    /// <summary>
+    ///   Destructor
+    /// </summary>
+    destructor Destroy; override;
+  published
+    /// <summary>
+    ///   Start angle
+    /// </summary>
+    property StartAngle: Single read FStartAngle write SetStartAngle;
+    /// <summary>
+    ///   End angle
+    /// </summary>
+    property EndAngle: Single read FEndAngle write SetEndAngle;
+  end;
+
+implementation
+
+uses
+  Winapi.GDIPOBJ, Winapi.GDIPAPI, System.Math, Vcl.Themes;
+
+//------------------------------------------------------------------------------
+// SET START ANGLE
+//------------------------------------------------------------------------------
+procedure TOBDCircularGauge.SetStartAngle(Value: Single);
+begin
+  if (FStartAngle <> Value) and (Value >= MIN_START_ANGLE) and (Value <= MAX_START_ANGLE) then
+  begin
+    // Set new start angle
+    FStartAngle := Value;
+    // Invalidate the background buffer
+    InvalidateBackground
+  end;
+end;
+
+//------------------------------------------------------------------------------
+// SET END ANGLE
+//------------------------------------------------------------------------------
+procedure TOBDCircularGauge.SetEndAngle(Value: Single);
+begin
+  if (FEndAngle <> Value) and (Value >= MIN_END_ANGLE) and (Value <= MAX_END_ANGLE) then
+  begin
+    // Set new end angle
+    FEndAngle := Value;
+    // Invalidate the background buffer
+    InvalidateBackground
+  end;
+end;
+
+//------------------------------------------------------------------------------
+// INVALIDATE BACKGROUND
+//------------------------------------------------------------------------------
+procedure TOBDCircularGauge.InvalidateBackground;
+var
+  SS: TCustomStyleServices;
+  Graphics: TGPGraphics;
+  Size, X, Y: Single;
+  GaugeRect: TGPRectF;
+  Brush: TGPBrush;
+begin
+  // If VCL styles is available and enabled, then draw the VCL Style background
+  // so it matches the active style background like on the Form or a Panel.
+  if TStyleManager.IsCustomStyleActive then
+  begin
+    SS := StyleServices;
+    // Draw the styled background
+    SS.DrawElement(FBackgroundBuffer.Canvas.Handle, SS.GetElementDetails(tpPanelBackground), ClientRect);
+  end else
+  // Otherwise fill the background with the color.
+  with FBackgroundBuffer.Canvas do
+  begin
+    // Use the component color
+    Brush.Color := Self.Color;
+    // Use a solid brush
+    Brush.Style := bsSolid;
+    // Fill the background with the component color
+    FillRect(Rect(0, 0, ClientWidth, ClientHeight));
+  end;
+
+  // Initialize GDI+ Graphics object
+  Graphics := TGPGraphics.Create(Buffer.Canvas.Handle);
+  try
+    // Set smoothing mode to anti-alias
+    Graphics.SetSmoothingMode(SmoothingModeAntiAlias);
+    // Calculate gauge size and position based on control's aspect ratio
+    Size := Min(Width, Height);
+    X := (Width - Size) / 2;
+    Y := (Height - Size) / 2;
+
+    // Get the rectangle for the gauge
+    GaugeRect := MakeRect(X, Y, Size, Size);
+
+    // Create the background brush
+    Brush := TGPSolidBrush.Create(clWhite);
+    try
+      // Fill the gauge background
+      Graphics.FillEllipse(Brush, GaugeRect);
+    finally
+      // Free the background brush object
+      Brush.Free;
+    end;
+
+    // Draw the ticks
+    // Draw the numbers?
+
+  finally
+    // Free the GDI+ Graphics object
+    Graphics.Free;
+  end;
+end;
+
+//------------------------------------------------------------------------------
+// PAINT BUFFER
+//------------------------------------------------------------------------------
+procedure TOBDCircularGauge.PaintBuffer;
+begin
+  // Copy the background buffer to the main buffer, by buffering the background
+  // and only updating the background buffer when the background is changed
+  // allows us to just copy the background buffer, which speeds up our PaintBuffer
+  // resulting in less CPU consumption and allowing higher framerates.
+  BitBlt(Buffer.Canvas.Handle, 0, 0, ClientWidth, ClientHeight, FBackgroundBuffer.Canvas.Handle, 0,  0, SRCCOPY);
+
+end;
+
+//------------------------------------------------------------------------------
+// CONSTRUCTOR
+//------------------------------------------------------------------------------
+constructor TOBDCircularGauge.Create(AOwner: TComponent);
+begin
+  // Call inherited constructor
+  inherited Create(AOwner);
+  // Create background buffer
+  FBackgroundBuffer := TBitmap.Create;
+  // Set the background buffer pixel format
+  FBackgroundBuffer.PixelFormat := pf32bit;
+  // Set default start angle
+  FStartAngle := DEFAULT_START_ANGLE;
+  // Set default end angle
+  FEndAngle := DEFAULT_END_ANGLE;
+  // Invalidate background buffer
+  InvalidateBackground;
+end;
+
+//------------------------------------------------------------------------------
+// DESTRUCTOR
+//------------------------------------------------------------------------------
+destructor TOBDCircularGauge.Destroy;
+begin
+  // Free background buffer
+  FBackgroundBuffer.Free;
+  // Call inherited destructor
+  inherited Destroy;
+end;
+
+end.
---
+++ OBD.CustomControl.pas
@@ -0,0 +1,239 @@
+﻿//------------------------------------------------------------------------------
+// UNIT           : OBD.CustomControl.pas
+// CONTENTS       : Base Custom Control Component
+// VERSION        : 1.0
+// TARGET         : Embarcadero Delphi 11 or higher
+// AUTHOR         : Ernst Reidinga (ERDesigns)
+// STATUS         : Open source under Apache 2.0 library
+// COMPATIBILITY  : Windows 7, 8/8.1, 10, 11
+// RELEASE DATE   : 20/03/2024
+// NOTE           : This component serves as a base for other visual components,
+//                  it has a buffer and is drawn at a specified frame-rate, which
+//                  makes it ideal for components like Gauges and Displays that
+//                  need to be redrawn regularly.
+//------------------------------------------------------------------------------
+unit OBD.CustomControl;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Vcl.Controls, WinApi.Windows, Winapi.Messages, Vcl.Graphics;
+
+//------------------------------------------------------------------------------
+// CONSTANTS
+//------------------------------------------------------------------------------
+const
+  /// <summary>
+  ///   Default frames per second
+  /// </summary>
+  DEFAULT_FPS = 30;
+
+//------------------------------------------------------------------------------
+// CLASSES
+//------------------------------------------------------------------------------
+type
+  TOBDCustomControl = class(TCustomControl)
+  private
+    /// <summary>
+    ///   Buffer (This is the canvas we draw on)
+    /// </summary>
+    FBuffer: TBitmap;
+    /// <summary>
+    ///   Update rect (Invalidated rectangle)
+    /// </summary>
+    FUpdateRect: TRect;
+    /// <summary>
+    ///   Handle of the FPS timer
+    /// </summary>
+    FTimerHandle: UINT_PTR;
+  private
+    /// <summary>
+    ///   Frames per second
+    /// </summary>
+    FFramesPerSecond: Integer;
+
+    /// <summary>
+    ///   Set frames per second
+    /// </summary>
+    procedure SetFramesPerSecond(Value: Integer);
+  private
+    /// <summary>
+    ///   WM_TIMER message handler
+    /// </summary>
+    procedure WMTimeStep(var Msg: TMessage); message WM_TIMER;
+    /// <summary>
+    ///   WM_PAINT message handler
+    /// </summary>
+    procedure WMPaint(var Msg: TWMPaint); message WM_PAINT;
+  protected
+    /// <summary>
+    ///   Buffer (This is the canvas we draw on)
+    /// </summary>
+    property Buffer: TBitmap read FBuffer;
+
+    /// <summary>
+    ///   Override CreateParams method
+    /// </summary>
+    procedure CreateParams(var Params: TCreateParams); override;
+    /// <summary>
+    ///   Override Paint method
+    /// </summary>
+    procedure Paint; override;
+    /// <summary>
+    ///   Override Resize method
+    /// </summary>
+    procedure Resize; override;
+  protected
+    /// <summary>
+    ///   Paint buffer
+    /// </summary>
+    procedure PaintBuffer; virtual; abstract;
+    /// <summary>
+    ///   Invalidate buffer
+    /// </summary>
+    procedure InvalidateBuffer; virtual;
+  public
+    /// <summary>
+    ///   Constructor
+    /// </summary>
+    constructor Create(AOwner: TComponent); virtual;
+    /// <summary>
+    ///   Destructor
+    /// </summary>
+    destructor Destroy; override;
+  published
+    /// <summary>
+    ///   Frames per second
+    /// </summary>
+    property FramesPerSecond: Integer read FFramesPerSecond write SetFramesPerSecond default DEFAULT_FPS;
+  end;
+
+implementation
+
+//------------------------------------------------------------------------------
+// SET FRAMES PER SECOND
+//------------------------------------------------------------------------------
+procedure TOBDCustomControl.SetFramesPerSecond(Value: Integer);
+begin
+  if (FFramesPerSecond <> Value) and (Value >= 1) and (Value < 100) then
+  begin
+    // Update the FPS
+    FFramesPerSecond := Value;
+    // Kill the timer
+    if (FTimerHandle <> 0) then KillTimer(Handle, FTimerHandle);
+    // Create new timer
+    if not (csDesigning in ComponentState) then FTimerHandle := SetTimer(Handle, 1, 1000 div FFramesPerSecond, nil);
+  end;
+end;
+
+//------------------------------------------------------------------------------
+// WM_TIMER MESSAGE HANDLER
+//------------------------------------------------------------------------------
+procedure TOBDCustomControl.WMTimeStep(var Msg: TMessage);
+begin
+  // Trigger a repaint
+  Invalidate;
+end;
+
+//------------------------------------------------------------------------------
+// WM_PAINT MESSAGE HANDLER
+//------------------------------------------------------------------------------
+procedure TOBDCustomControl.WMPaint(var Msg: TWMPaint);
+begin
+  inherited;
+  // Retrieve the invalidated rectangle
+  if not GetUpdateRect(Handle, FUpdateRect, False) then
+  // If no update region, default to the entire client area
+  FUpdateRect := Rect(0, 0, Width, Height);
+end;
+
+//------------------------------------------------------------------------------
+// CREATE PARAMS
+//------------------------------------------------------------------------------
+procedure TOBDCustomControl.CreateParams(var Params: TCreateParams);
+begin
+  // Adjust window style to avoid unnecessary redraws on size changes,
+  // optimizing performance for custom drawing.
+  with Params do Style := Style and not (CS_HREDRAW or CS_VREDRAW);
+end;
+
+//------------------------------------------------------------------------------
+// PAINT
+//------------------------------------------------------------------------------
+procedure TOBDCustomControl.Paint;
+var
+  X, Y, W, H: Integer;
+begin
+  // Draw the buffer to the component canvas
+  X := FUpdateRect.Left;
+  Y := FUpdateRect.Top;
+  W := FUpdateRect.Right - FUpdateRect.Left;
+  H := FUpdateRect.Bottom - FUpdateRect.Top;
+  if (W <> 0) and (H <> 0) then
+    // Only update invalidated part
+    BitBlt(Canvas.Handle, X, Y, W, H, FBuffer.Canvas.Handle, X,  Y, SRCCOPY)
+  else
+    // Repaint the whole buffer to the surface
+    BitBlt(Canvas.Handle, 0, 0, ClientWidth, ClientHeight, FBuffer.Canvas.Handle, X,  Y, SRCCOPY);
+end;
+
+//------------------------------------------------------------------------------
+// RESIZE
+//------------------------------------------------------------------------------
+procedure TOBDCustomControl.Resize;
+begin
+  // Call inherited resize method
+  inherited Resize;
+  // Update the size of the buffer
+  FBuffer.SetSize(Width, Height);
+  // Invalidate the buffer
+  InvalidateBuffer;
+end;
+
+//------------------------------------------------------------------------------
+// INVALIDATE BUFFER
+//------------------------------------------------------------------------------
+procedure TOBDCustomControl.InvalidateBuffer;
+begin
+  // Execute the Paint Buffer method. This method is an alternative for the
+  // default invalidate method, because we are drawing to the component canvas
+  // when the timer is fired (Fires x times per second).
+  PaintBuffer;
+  // When we are using this component in Design Mode (In the Delphi IDE)
+  // we want to invalidate the control too, because the timer wont fire
+  // in Design mode.
+  if (csDesigning in ComponentState) then Invalidate;
+end;
+
+//------------------------------------------------------------------------------
+// CONSTRUCTOR
+//------------------------------------------------------------------------------
+constructor TOBDCustomControl.Create(AOwner: TComponent);
+begin
+  // Call inherited constructor
+  inherited Create(AOwner);
+  // Prevent background erasure for smoother rendering and reduced flickering.
+  ControlStyle := ControlStyle + [csOpaque];
+  // Create Buffer
+  FBuffer := TBitmap.Create;
+  // Set the buffer pixel format
+  FBuffer.PixelFormat := pf32bit;
+  // Set initial FPS
+  FFramesPerSecond := DEFAULT_FPS;
+end;
+
+//------------------------------------------------------------------------------
+// DESTRUCTOR
+//------------------------------------------------------------------------------
+destructor TOBDCustomControl.Destroy;
+begin
+  // Free buffer
+  FBuffer.Free;
+  // Kill the timer
+  if FTimerHandle <> 0 then KillTimer(Handle, FTimerHandle);
+  // Call inherited destructor
+  inherited Destroy;
+end;
+
+end.
+
