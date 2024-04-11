bee827f9ff2fcb16a89d857bd370f09e107bf585
ERDesigns Software <ernstreidinga85@gmail.com>
2024-04-11 19:22:01 +0200

    progress
    Moved the single package to two separate packages, one for designtime and another for runtime. Also started on adding Radio Calculators for common car radio's (Ford M/V, Renault, VP1/VP2)

---
+++ OBD.RadioCode.pas
@@ -0,0 +1,100 @@
+﻿//------------------------------------------------------------------------------
+// UNIT           : OBD.RadioCode.Renault.pas
+// CONTENTS       : Renault Radio Code Calculator
+// VERSION        : 1.0
+// TARGET         : Embarcadero Delphi 11 or higher
+// AUTHOR         : Ernst Reidinga (ERDesigns)
+// STATUS         : Open source under Apache 2.0 library
+// COMPATIBILITY  : Windows 7, 8/8.1, 10, 11
+// RELEASE DATE   : 10/04/2024
+//------------------------------------------------------------------------------
+unit OBD.RadioCode;
+
+interface
+
+uses
+  Winapi.Windows, System.SysUtils;
+
+//------------------------------------------------------------------------------
+// INTERFACES
+//------------------------------------------------------------------------------
+type
+  /// <summary>
+  ///   OBD RadioCode (INTERFACE)
+  /// </summary>
+  IOBDRadioCode = interface
+    ['{F2733032-6446-4E7B-9477-4AD9C67F1CB3}']
+    /// <summary>
+    ///   Get description for this calculator
+    /// </summary>
+    function GetDescription: string;
+    /// <summary>
+    ///   Radio Code input validator
+    /// </summary>
+    /// <param name="Input">
+    ///   The serial number or any other needed input to calculate the
+    ///   radio code.
+    /// </param>
+    ///  /// <param name="ErrorMessage">
+    ///   A error message (Optional) that descibes why the input is invalid.
+    /// </param>
+    function Validate(const Input: string; var ErrorMessage: string): Boolean;
+    /// <summary>
+    ///   Radio Code Calculator
+    /// </summary>
+    /// <param name="Input">
+    ///   The serial number or any other needed input to calculate the
+    ///   radio code.
+    /// </param>
+    /// <param name="Output">
+    ///   The calculated radio code.
+    /// </param>
+    /// <param name="ErrorMessage">
+    ///   A error message (Optional) that descibes why the input is invalid.
+    /// </param>
+    function Calculate(const Input: string; var Output: string; var ErrorMessage: string): Boolean;
+  end;
+
+//------------------------------------------------------------------------------
+// CLASSES
+//------------------------------------------------------------------------------
+type
+  /// <summary>
+  ///   OBD RadioCode (CLASS)
+  /// </summary>
+  TOBDRadioCode = class(TInterfacedObject, IOBDRadioCode)
+  public
+    /// <summary>
+    ///   Get description for this calculator
+    /// </summary>
+    function GetDescription: string; virtual; abstract;
+    /// <summary>
+    ///   Radio Code input validator
+    /// </summary>
+    /// <param name="Input">
+    ///   The serial number or any other needed input to calculate the
+    ///   radio code.
+    /// </param>
+    ///  /// <param name="ErrorMessage">
+    ///   A error message (Optional) that descibes why the input is invalid.
+    /// </param>
+    function Validate(const Input: string; var ErrorMessage: string): Boolean; virtual; abstract;
+    /// <summary>
+    ///   Radio Code Calculator
+    /// </summary>
+    /// <param name="Input">
+    ///   The serial number or any other needed input to calculate the
+    ///   radio code.
+    /// </param>
+    /// <param name="Output">
+    ///   The calculated radio code.
+    /// </param>
+    /// <param name="ErrorMessage">
+    ///   A error message (Optional) that descibes why the input is invalid.
+    /// </param>
+    function Calculate(const Input: string; var Output: string; var ErrorMessage: string): Boolean; virtual; abstract;
+  end;
+
+implementation
+
+end.
